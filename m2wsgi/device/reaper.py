fe94f941b234325bdbd78b30cea15d8b07f48511
Ryan Kelly <ryan@rfk.id.au>
2011-03-03 15:55:48 +1100

    refactor reaper device into a class

--- m2wsgi/device/reaper.py
+++ m2wsgi/device/reaper.py
@@ -21,13 +21,9 @@ prejudice.
 By default the reaper device stays alive and repeatedly polls for connections.
 To run a single cleanup sweep and then exit, specify the --oneshot option.
 
-This device is currently very simplistic, as the data from the control port
-isn't very comprehensive.  It supports only a max connection lifetime.
-
-I hope to enhance mongrel2's control port with more information about each
-connection (e.g. time since last write, number of bytes read) and then beef
-up the timeout options offered by this device.
-
+Current timeout options include max connection lifetime and max idle time.
+You will need to be running a post-1.5 version of mongrel2 to use the idle
+time option.
 
 """
 
@@ -38,44 +34,90 @@ from textwrap import dedent
 import zmq
 from m2wsgi.io.standard import Connection
 
-def reaper(ctrl_sock,max_lifetime=60,oneshot=False):
-    if isinstance(ctrl_sock,basestring):
-        ctrl_sock = Connection.makesocket(zmq.REQ,ctrl_sock)
-    next_reap_time = 0
-    while True:
-        time.sleep(next_reap_time)
-        next_reap_time = max_lifetime
-        ctrl_sock.send("status net")
-        conns = json.loads(ctrl_sock.recv())
+
+class Reaper(object):
+
+    def __init__(self,ctrl_sock,recv_sock=None,**opts):
+        if isinstance(ctrl_sock,basestring):
+            ctrl_sock = Connection.makesocket(zmq.REQ,ctrl_sock)
+        if isinstance(recv_sock,basestring):
+            recv_sock = Connection.makesocket(zmq.PUB,recv_sock)
+        self.stopped = False
+        self.ctrl_sock = ctrl_sock
+        self.recv_sock = recv_sock
+        self.max_lifetime = opts.pop("max_lifetime",60)
+        self.max_idle_time = opts.pop("max_idle_time",30)
+        if opts:
+            raise TypeError("unknown options: %s" % (opts.keys(),))
+
+    def run(self):
+        next_reap_time = 0
+        while not self.stopped:
+            time.sleep(next_reap_time)
+            next_reap_time = self.reap()
+
+    def stop(self):
+        self.stopped = True
+
+    def reap(self):
+        next_reap_time = min(self.max_lifetime,self.max_idle_time)
+        self.ctrl_sock.send("status net")
+        conns = json.loads(self.ctrl_sock.recv())
         for (k,v) in conns.iteritems():
             if k == "total":
                 continue
-            (fd,last_ping) = v
-            if last_ping >= max_lifetime:
-                ctrl_sock.send("kill " + str(k))
-                ctrl_sock.recv()
-            else:
-                time_left = max_lifetime - last_ping
+            #  Older mongrel2 returned a list.  Newer
+            #  versions return a dict with more info.
+            if isinstance(v,list):
+                v = dict(fd=v[0],last_ping=v[1])
+            if self.max_lifetime is not None:
+                last_ping = v.get("last_ping",0)
+                if last_ping >= self.max_lifetime:
+                    self.ctrl_sock.send("kill " + str(k))
+                    self.ctrl_sock.recv()
+                    continue
+                time_left = self.max_lifetime - last_ping
                 if time_left < next_reap_time:
                     next_reap_time = time_left
-        if oneshot:
-            break
+            if self.max_idle_time is not None:
+                last_activity = min(v.get("last_read",0),
+                                    v.get("last_write",0))
+                if last_activity >= self.max_idle_time:
+                    self.ctrl_sock.send("kill " + str(k))
+                    self.ctrl_sock.recv()
+                    continue
+                time_left = self.max_idle_time - last_activity
+                if time_left < next_reap_time:
+                    next_reap_time = time_left
+        return next_reap_time
+
 
 if __name__ == "__main__":
     import optparse
     op = optparse.OptionParser(usage=dedent("""
-    usage:  m2wsgi.device.reaper [options] [ctrl_spec]
+    usage:  m2wsgi.device.reaper [options] [ctrl_spec] [recv_spec]
     """))
     op.add_option("","--max-lifetime",type="int",default=60,
                   help="max connection lifetime in seconds")
+    op.add_option("","--max-idle-time",type="int",default=30,
+                  help="max idle connection time in seconds")
     op.add_option("","--oneshot",action="store_true",
                   help="only reap once, then exit")
     (opts,args) = op.parse_args()
+    recv_sock = None
     if not args:
         ctrl_sock = "ipc://run/control"
-    elif len(args) > 1:
-         raise ValueError("reaper expects a single argument")
-    else:
+    elif len(args) == 1:
         ctrl_sock = args[0]
-    reaper(ctrl_sock,**opts.__dict__)
+    elif len(args) == 2:
+        ctrl_sock = args[0]
+        recv_sock = args[1]
+    else:
+         raise ValueError("reaper expects at most two arguments")
+    oneshot = opts.__dict__.pop("oneshot")
+    r = Reaper(ctrl_sock,recv_sock,**opts.__dict__)
+    if oneshot:
+        r.reap()
+    else:
+        r.run()
 
